4a6df08b0a39e8b2e73d5d43506ad9f62856227d
Fouad Smaoui <Fouad.Smaoui@ensam.eu>
2024-06-25 23:11:39 +0200

    folders refactoring

--- Linux-For-Robotics/catkin_ws/src/robot_control/lists.py
+++
@@ -1,7 +0,0 @@
-from robot_control_class import RobotControl
-robotcontrol  = RobotControl()
-laser_data = robotcontrol .get_laser_full()
-
-print ("Position 0: ", laser_data[0])
-print ("Position 360: ", laser_data[360])
-print ("Position 719: ", laser_data[719])
